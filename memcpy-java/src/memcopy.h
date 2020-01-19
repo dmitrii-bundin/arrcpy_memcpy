4903b8a5ac72fe144be7d62214977a866ddb71ee
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2020-01-19 19:45:34 +0300

    avx store implementation

--- memcpy-java/src/memcopy.h
+++ memcpy-java/src/memcopy.h
@@ -5,6 +5,8 @@ void *avx_nt_memcpy(void *restrict, const void *restrict, size_t);
 
 void *avx_nt_memcpy_forward(void *restrict, const void *restrict, size_t);
 
-void *avx_copy_cache_line(void *dest, const void *src, size_t sz);
+void avx_ntcopy_cache_line(void *dest, const void *src);
+
+void avx_ntcopy_64_two_cache_lines(void *dest, const void *src);
 
 #endif //MEMCOPY_H
