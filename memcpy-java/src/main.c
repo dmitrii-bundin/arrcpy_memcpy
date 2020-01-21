73b602ff573dcf4ef337c3de14b9f741553bde3c
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2020-01-21 07:10:17 +0300

    rdpmc removed

--- memcpy-java/src/main.c
+++ memcpy-java/src/main.c
@@ -1,53 +1,62 @@
+#include <stdlib.h>
+#include <inttypes.h>
+#include <x86intrin.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include "benchmark.h"
 #include "memcopy.h"
 
-#define ITERATIONS 100
-#define BUF_SIZE 4 * 1024 * 1024
+#define ITERATIONS 10000000
 
-_Alignas(4096) char src[BUF_SIZE];
-_Alignas(4096) char dest[BUF_SIZE];
+//As @HadiBrais noted, there might be an issue with 4K aliasing
+_Alignas(64) char src[128];
+_Alignas(64) char dest[128];
 
-void run_benchmark(unsigned runs, unsigned run_iterations,
-                    void *(*fn)(void *, const void*, size_t), void *dest, const void* src, size_t sz){
-    unsigned current_run = 1;
-    while(current_run <= runs){
-        uint64_t time = benchmark_copy_function(run_iterations, fn, dest, src, sz);
-        printf("Run %d result: %lu\n", current_run, time);
-        current_run++;
-    }
-}
+static void __run_benchmark(unsigned runs, unsigned run_iterations,
+                    void (*fn)(void *, const void*), void *dest, const void* src);
+
+#define run_benchmark(runs, run_iterations, fn, dest, src) \
+    do{\
+        printf("Benchmarking " #fn "\n");\
+        __run_benchmark(runs, run_iterations, fn, dest, src);\
+    }while(0)
+
 int main(void){
     int fd = open("/dev/urandom", O_RDONLY);
     read(fd, src, sizeof src);
-    printf("%s\n", src);
-    avx_nt_memcpy_forward(dest, src, sizeof src);
-    printf("%s\n", dest);
 
-    run_benchmark(20, ITERATIONS, avx_nt_memcpy, dest, src, sizeof src);
-    // unsigned iterations = 20;
-    // unsigned current_iteration = 1;
-    // while(current_iteration <= iterations){
-    //     uint64_t time = benchmark_copy_function(ITERATIONS, avx_nt_memcpy, dest, src, sizeof src);
-    //     printf("Iteration %d result: %lu\n", current_iteration, time);
-    //     current_iteration++;
-    // }
+    // run_benchmark(20, ITERATIONS, avx_copy_64, dest, src);
+    run_benchmark(20, ITERATIONS, avx_copy_32_32, dest, src);
+}
 
-    // unsigned copy_cache_line_iteration_2 = 20;
-    // unsigned copy_cache_line_current_iteration_2 = 1;
-    // while(copy_cache_line_current_iteration_2 <= copy_cache_line_iteration_2){
-    //     uint64_t time = benchmark_copy_function(ITERATIONS, avx_copy_cache_line, dest, src, sizeof src);
-    //     printf("Iteration %d result: %lu\n", copy_cache_line_current_iteration_2, time);
-    //     copy_cache_line_current_iteration_2++;
-    // }
-    
-    // unsigned copy_cache_line_iteration = 20;
-    // unsigned copy_cache_line_current_iteration = 1;
-    // while(copy_cache_line_current_iteration <= copy_cache_line_iteration){
-    //     uint64_t time = benchmark_copy_function(ITERATIONS, avx_copy_cache_line, dest, src, sizeof src);
-    //     printf("Iteration %d result: %lu\n", copy_cache_line_current_iteration, time);
-    //     copy_cache_line_current_iteration++;
-    // }
+static inline void benchmark_2cache_lines_copy_function(unsigned iterations, void (*fn)(void *, const void *),
+                                               void *restrict dest, const void *restrict src){
+    while(iterations --> 0){
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+        fn(dest, src);
+    }
+}
+
+static void __run_benchmark(unsigned runs, unsigned run_iterations,
+                    void (*fn)(void *, const void*), void *dest, const void* src){
+    unsigned current_run = 1;
+    while(current_run <= runs){
+        benchmark_2cache_lines_copy_function(run_iterations, fn, dest, src);
+        printf("Run %d finished\n", current_run);
+        current_run++;
+    }
 }
--- memcpy-java/src/memcopy.h
+++ memcpy-java/src/memcopy.h
@@ -5,8 +5,8 @@ void *avx_nt_memcpy(void *restrict, const void *restrict, size_t);
 
 void *avx_nt_memcpy_forward(void *restrict, const void *restrict, size_t);
 
-void avx_ntcopy_cache_line(void *dest, const void *src);
+void avx_copy_64(void *dest, const void *src);
 
-void avx_ntcopy_64_two_cache_lines(void *dest, const void *src);
+void avx_copy_32_32(void *dest, const void *src);
 
 #endif //MEMCOPY_H
